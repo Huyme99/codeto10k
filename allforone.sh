a29906dc6ade4d7e862ab1d70031093224bc2e5e
Huyme99 <43411488+Huyme99@users.noreply.github.com>
2024-10-08 03:45:28 +0700

    Update allforone.sh

--- allforone.sh
+++ allforone.sh
@@ -34,36 +34,25 @@ edit_query() {
     nano query.txt  # Mở tập tin query.txt bằng trình soạn thảo nano
 }
 
+clear  # Xóa màn hình terminal
+
 # Hàm để chọn lựa chọn 1 hoặc 2
 chooseOption() {
-    read -p "Chọn 1 để chạy file setup.sh hoặc chọn 2 để hủy bỏ (1/2): " choice
+    read -p "Chọn 1 để chỉnh sửa proxy.txt hoặc chọn 2 để chỉnh sửa query.txt (1/2): " choice
     case $choice in
         1)
-            echo "Đang chạy file setup.sh từ link..."
-            node setup1.js
+            show "Chỉnh sửa proxy.txt..."
+            edit_proxy  # Gọi hàm chỉnh sửa proxy.txt
             ;;
         2)
-            echo "Hủy bỏ."
+            show "Chỉnh sửa query.txt..."
+            edit_query  # Gọi hàm chỉnh sửa query.txt
             ;;
         *)
-            echo "Lựa chọn không hợp lệ."
+            show "Lựa chọn không hợp lệ."
             ;;
     esac
 }
-
-# Hàm để chọn và chỉnh sửa proxy và query
-chooseAndEditOptions() {
-    show "Chỉnh sửa proxy.txt..."
-    edit_proxy  # Gọi hàm chỉnh sửa proxy.txt
-
-    show "Chỉnh sửa query.txt..."
-    edit_query  # Gọi hàm chỉnh sửa query.txt
-
-    echo "Bạn đã thêm thành công proxy và query"
-}
-
-# Thực hiện chọn và chỉnh sửa proxy và query
-chooseAndEditOptions
 
 # Thực thi chương trình
 run() {
