b6a612e17d4f625dfd8367e6f1c66e4a9cd92643
Huyme99 <43411488+Huyme99@users.noreply.github.com>
2024-10-08 02:51:24 +0700

    Update allforone.sh

--- allforone.sh
+++ allforone.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 echo
-curl -s https://raw.githubusercontent.com/Huyme99/logo/refs/heads/main/logo.sh
+# Lấy nội dung của logo và hiển thị
+logo_content=$(curl -s https://raw.githubusercontent.com/Huyme99/logo/refs/heads/main/logo.sh)
+echo -e "$logo_content"
 
 show() {
     echo -e "\033[1;34m$1\033[0m"
@@ -12,11 +14,20 @@ echo
 sudo apt update
 sudo apt install -y git
 echo
+
+# Lưu vị trí hiện tại của con trỏ in
+tput sc
+
 show "Installing Node.js..."
+# Thực thi mã để cài đặt Node.js
 wget -O - https://raw.githubusercontent.com/Huyme99/Installation/refs/heads/main/node.sh | bash
 echo
 
-clear  # Xóa màn hình terminal
+# Di chuyển con trỏ in về đúng vị trí của logo sau khi clear màn hình
+tput rc
+
+# Xóa màn hình terminal
+clear
 
 # Định nghĩa hàm cho lựa chọn số 1 và số 2
 chooseOption() {
