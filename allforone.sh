bfaa92e973f099b5a5fda0f57ab55d9c93f32f9b
Huyme99 <43411488+Huyme99@users.noreply.github.com>
2024-10-08 03:05:01 +0700

    Update allforone.sh

--- allforone.sh
+++ allforone.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-echo
 # Lấy nội dung của logo và hiển thị
-logo_content=$(curl -s https://raw.githubusercontent.com/Huyme99/logo/refs/heads/main/logo.sh)
-echo -e "$logo_content"
+curl -s https://raw.githubusercontent.com/Huyme99/logo/refs/heads/main/logo.sh | bash
+sleep 3
 
 show() {
     echo -e "\033[1;34m$1\033[0m"
@@ -15,21 +14,27 @@ sudo apt update
 sudo apt install -y git
 echo
 
-# Lưu vị trí hiện tại của con trỏ in
-tput sc
-
 show "Installing Node.js..."
 # Thực thi mã để cài đặt Node.js
 wget -O - https://raw.githubusercontent.com/Huyme99/Installation/refs/heads/main/node.sh | bash
 echo
 
-# Di chuyển con trỏ in về đúng vị trí của logo sau khi clear màn hình
-tput rc
+# Tạo hai tập tin proxy.txt và query.txt
+echo "Nội dung cho proxy.txt" > proxy.txt
+echo "Nội dung cho query.txt" > query.txt
 
-# Xóa màn hình terminal
-clear
 
-# Định nghĩa hàm cho lựa chọn số 1 và số 2
+# Hàm chỉnh sửa tập tin proxy.txt
+edit_proxy() {
+    nano proxy.txt  # Mở tập tin proxy.txt bằng trình soạn thảo nano
+}
+
+# Hàm chỉnh sửa tập tin query.txt
+edit_query() {
+    nano query.txt  # Mở tập tin query.txt bằng trình soạn thảo nano
+}
+
+# Hàm để chọn lựa chọn 1 hoặc 2
 chooseOption() {
     read -p "Chọn 1 để chạy file setup.sh hoặc chọn 2 để hủy bỏ (1/2): " choice
     case $choice in
@@ -45,6 +50,20 @@ chooseOption() {
             ;;
     esac
 }
+
+# Hàm để chọn và chỉnh sửa proxy và query
+chooseAndEditOptions() {
+    show "Chỉnh sửa proxy.txt..."
+    edit_proxy  # Gọi hàm chỉnh sửa proxy.txt
+
+    show "Chỉnh sửa query.txt..."
+    edit_query  # Gọi hàm chỉnh sửa query.txt
+
+    echo "Bạn đã thêm thành công proxy và query"
+}
+
+# Thực hiện chọn và chỉnh sửa proxy và query
+chooseAndEditOptions
 
 # Thực thi chương trình
 run() {
